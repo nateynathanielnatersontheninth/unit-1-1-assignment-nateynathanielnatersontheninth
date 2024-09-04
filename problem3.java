18a329f31932cc40391086636f9d9fe3e90b1734
nateynathanielnatersontheninth <nltruong101@student.hbuhsd.edu>
2024-09-04 10:55:34 -0700

    im skibidi satisfied with my work

--- problem3.java
+++ problem3.java
@@ -1,1 +1,10 @@
-
+public class problem3
+{
+    public static void main (String[] args)
+    {
+        System.out.println("=========");
+        System.out.println("= 0   0 =  ");
+        System.out.println("=   v   = ");
+        System.out.println("=========");
+    }
+}

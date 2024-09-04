0a6698fe38cb9086844f4e4d4f898eb25be620ca
nateynathanielnatersontheninth <nltruong101@student.hbuhsd.edu>
2024-09-04 10:45:28 -0700

    skibidi

--- problem1.java
+++ problem1.java
@@ -1,1 +1,7 @@
-
+public class problem1
+{
+    public static void main (String[] args)
+    {
+        System.out.print(" LeMickey James ")
+    }
+}
--- problem2.java
+++ problem2.java
@@ -1,1 +1,8 @@
+public class SecondProgram
+{
+    public static void main (String[] args)
+    {
+        System.out.println(" Coding is fun ")
+    }
+}
 
